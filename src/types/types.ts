68a55a40230327ed922500b8cc5b3e6197d40101
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-07-07 14:04:09 +0300

    types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,9 +1,25 @@
-
+export interface RUser {
+    first_name: string;
+    last_name: string;
+    user_name: string;
+    contact_phone: string;
+    email: string;
+    address: string;
+    password: string;
+  }
+  export interface Luser {
+      username: string;
+      password: string;
+      role: string;
+      token: string;
+  }
  export interface TUser {
     id: number;
-    fullname: string;
+    first_name: string;
+    last_name: string;
+    user_name: string;
     email: string;
-    phone: string;
+    contact_phone: string;
     address: string;
 }
 export interface TBooking {
@@ -23,3 +39,13 @@ export interface TPayment {
     status: string;
     paymentDate: string;
 }
+
+export interface LoginResponse {
+    user: {
+      username: string;
+      role: string;
+      // Other user data as needed
+    };
+    role: Luser['role'];
+    token: string;
+  }
