9bd869adfab5cb57134f327984f0ac7eb2828ea1
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-07-08 18:01:54 +0300

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,44 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/header'
-import Login from './pages/login';
-import Contact from './pages/contact';
-import Register from './pages/register';
-import Footer from './components/footer';
-import LandingPage from './pages/landingPage';
-import Fleets from './pages/fleet';
-import AboutUs from './pages/about';
-import Explore from './pages/explore';
-import BookingForm from './userDashboard/bookingForm';
-import PaymentPage from './userDashboard/payment';
-import BookingConfirmation from './userDashboard/bookingForm';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/header";
+import Login from "./pages/login";
+import Contact from "./pages/contact";
+import Register from "./pages/register";
+import Footer from "./components/footer";
+import LandingPage from "./pages/landingPage";
+import Fleets from "./pages/fleet";
+import AboutUs from "./pages/about";
+import Explore from "./pages/explore";
+import BookingForm from "./userDashboard/bookingForm";
+import PaymentPage from "./userDashboard/payment";
+import BookingConfirmation from "./userDashboard/bookingForm";
 // import UserApp from '../src/userDashboard/app.tsx';
-import { Navigation } from './userDashboard/navigation';
-import { Dashboard } from './userDashboard/dashboard';
-import { ProfilePage } from './userDashboard/profile';
-import { ThankYou } from './userDashboard/thankyou';
+import { Dashboard } from "./userDashboard/dashboard";
+import { ThankYou } from "./userDashboard/thankyou";
+import Profile from "./userDashboard/profile";
 
-
-const App: React.FC = () => {
+const App = () => {
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow">
           <Routes>
-            <Route path='/' element={<LandingPage />} />
+            <Route path="/" element={<LandingPage />} />
             <Route path="/home" element={<LandingPage />} />
             <Route path="/about-us" element={<AboutUs />} />
             <Route path="/explore" element={<Explore />} />
             <Route path="/fleets" element={<Fleets />} />
-            <Route path="/register" element={<Register />} /> 
+            <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/book/:vehicleId" element={<BookingForm />} />
             <Route path="/user-payment" element={<PaymentPage />} />
-            <Route path="/booking-confirmation" element={<BookingConfirmation />} />
+            <Route
+              path="/booking-confirmation"
+              element={<BookingConfirmation />}
+            />
             <Route path="/user-dashboard" element={<Dashboard />} />
-            <Route path="/user-profile" element={<ProfilePage />} />
+            <Route path="/user-profile" element={<Profile />} />
             <Route path="/payment" element={<PaymentPage />} />
             <Route path="/book/:vehicleId" element={<BookingForm />} />
             <Route path="/thank-you" element={<ThankYou />} />
@@ -50,7 +50,6 @@ const App: React.FC = () => {
   );
 };
 
-// Include error handling 
-
+// Include error handling
 
 export default App;
