ee80322f585bdae877a0f8da116b3b26460ce9c6
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-07-16 14:51:29 +0300

    color

--- src/adminDashboard/dashboard.tsx
+++ src/adminDashboard/dashboard.tsx
@@ -4,6 +4,7 @@ import bookingsAPI from '../features/bookings/bookingsApi';
 import Navigation from './navigation';
 import usersAPI from '../features/users/usersAPI';
 import VehiclesAPI from '../features/vehicles/vehicleAPI';
+import { FaUsers, FaCar, FaCalendarAlt } from "react-icons/fa";
 
 export const Dashboard: React.FC = () => {
 
@@ -19,21 +20,24 @@ export const Dashboard: React.FC = () => {
   return (
     <div className="container mx-4 px-4 py-8">
               <h2 className="text-2xl font-bold mb-5">Summary</h2>
-      <div className='flex ml-20 gap-10'>
+      <div className='flex ml-20 font-bold text-2xl gap-10'>
                 {/* cards */}
-        <div className='flex bg-secondary text-white py-10 px-4 rounded mb-10 h-[200px] w-[300px]'>
-          Total Bookings: {bookings?.length} {bookings?.length === 1 ? 'record' : 'records'}
+        <div className='flex flex-col items-center bg-cards text-white  hover:bg-cyan-500  py-10 px-4 rounded mb-10 h-[200px] w-[300px]'>
+        <FaCalendarAlt /> <br/>
+          {bookings?.length} {bookings?.length === 1 ? 'booking' : 'Bookings'}
         </div >
-        <div className='flex bg-secondary text-white py-10 px-4 rounded mb-10 h-[200px] w-[300px]'>
-           Total Users: {users?.length} {users?.length === 1 ? 'record' : 'records'}
+        <div className='flex-col items-center flex bg-cards text-white  hover:bg-cyan-500  py-10 px-4 rounded mb-10 h-[200px] w-[300px]'>
+        <FaUsers /> <br/>
+           {users?.length} {users?.length === 1 ? 'user' : 'Users'}
         </div>
-        <div className='flex bg-secondary text-white py-10 px-4 rounded mb-10 h-[200px] w-[300px]'>
-        Total Vehicles: {vehicles?.length} {vehicles?.length === 1 ? 'record' : 'records'}
+        <div className='flex flex-col items-center bg-cards text-white  hover:bg-cyan-500  py-10 px-4 rounded mb-10 h-[200px] w-[300px]'>
+        <FaCar /> <br/>
+         {vehicles?.length} {vehicles?.length === 1 ? 'vehicle' : 'Vehicles'}
         </div>
       </div>
     <div>
         <h2 className="text-2xl font-bold mb-5">Bookings Data</h2>
-        <table className="min-w-full bg-base">
+        <table className="min-w-full text-white bg-cards">
           <thead>
             <tr>
               <th className="py-2 px-4 border-b-2 border-gray-300">Location</th>
@@ -47,7 +51,7 @@ export const Dashboard: React.FC = () => {
           </thead>
           <tbody>
             {bookings?.map((booking) => (
-              <tr key={booking.booking_id}>
+              <tr key={booking.booking_id} className="hover:bg-cyan-500 cursor-pointer">
                 <td className="py-2 px-4 border-b">{booking.location?.name}</td>
                 <td className="py-2 px-4 border-b">{new Date(booking.booking_date).toLocaleDateString()}</td>
                 <td className="py-2 px-4 border-b">{new Date(booking.return_date).toLocaleDateString()}</td>
@@ -56,7 +60,7 @@ export const Dashboard: React.FC = () => {
                 <td className="py-2 px-4 border-b">{booking.vehicle?.vehicleSpec?.manufacturer}</td>
                 
                 <td className="py-2 px-4 border-b">
-                  <button className="bg-teal-500 text-white py-1 px-3 rounded hover:bg-blue-600">View Details</button>
+                  <button className="bg-blue-100 text-black py-1 px-3 rounded hover:bg-blue-600">View Details</button>
                 </td>
               </tr>
             ))}
