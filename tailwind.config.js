5c4a24788b6854ab37442e6f81b45b7cc4280418
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-07-16 14:49:02 +0300

    color

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ export default {
 
       colors: {
         'secondary': '#003366',
+        'cards':'#116696',
         'gray-100': '#f5f5f5',
         'base': '#0D6D82', 
         'bg': '#000d1a',
