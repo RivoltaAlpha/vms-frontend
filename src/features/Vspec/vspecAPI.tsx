f4cb1ea3e837f4f3b29748f950808b8e743025f0
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-07-16 13:24:07 +0300

    vspec API

---
+++ src/features/Vspec/vspecAPI.tsx
@@ -0,0 +1,45 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import {  Vspec } from '../../types/types';
+
+export const VspecAPI = createApi({
+    reducerPath: 'VspecAPI',
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000',
+
+     prepareHeaders: (headers) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+          headers.set('Authorization', `Bearer ${token}`);
+        }
+        headers.set('Content-Type', 'application/json');
+        return headers;
+      },
+     }),
+    tagTypes: ['Vspec'],
+    endpoints: (builder) => ({
+        getVspec: builder.query<Vspec[], void>({
+            query: () => `/specifications`,
+            providesTags: ['Vspec'],
+        }),
+        getVspecById: builder.query<Vspec, number>({
+            query: (id) => `/specification/${id}`,
+            providesTags: ['Vspec'],
+        }),
+        createVspec: builder.mutation<Vspec, Partial<Vspec>>({
+            query: (newVspec) => ({
+                url: '/specification',
+                method: 'POST',
+                body: newVspec,
+            }),
+            invalidatesTags: ['Vspec'],
+        }),
+        deleteVspec: builder.mutation<{ success: boolean; id: number }, number>({
+            query: (id) => ({
+                url: `/delete-specification/${id}`,
+                method: 'DELETE',
+            }),
+            invalidatesTags: ['Vspec'],
+        }),
+    }),
+});
+
+export default VspecAPI;
