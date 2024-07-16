4e0df4ae9c1931b6e2ab7ae92aa8920f04a7859c
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-07-16 13:25:35 +0300

    img

--- src/pages/explore.tsx
+++ src/pages/explore.tsx
@@ -13,6 +13,7 @@ interface VehicleCardProps {
 const VehicleCard: React.FC<VehicleCardProps> = ({ vehicle, handleVehicleClick }) => (
   <div className="bg-white shadow-lg  rounded-lg overflow-hidden">
     <div className="px-6 items-center py-4">
+      <img src='./images/car3.png' className="w-full h-64 object-cover" alt="Car Image" />
       <p className="text-gray-700">Color: {vehicle.vehicleSpec.color}</p>
       <p className="text-gray-700">Engine Capacity: {vehicle.vehicleSpec.engine_capacity}</p>
       <p className="text-gray-700">Fuel Type: {vehicle.vehicleSpec.fuel_type}</p>
@@ -21,11 +22,11 @@ const VehicleCard: React.FC<VehicleCardProps> = ({ vehicle, handleVehicleClick }
       <p className="text-gray-700">Seating Capacity: {vehicle.vehicleSpec.seating_capacity}</p>
       <p className="text-gray-700">Transmission: {vehicle.vehicleSpec.transmission}</p>
       <p className="text-gray-700">Year: {vehicle.vehicleSpec.year}</p>
-      <p className="text-gray-700 font-bold mb-2">Availability: {vehicle.availability ? 'Available' : 'Not Available'}</p>
+      <p className="text-gray-700  mb-2">Availability: {vehicle.availability ? 'Available' : 'Not Available'}</p>
       <p className="text-gray-700">Rental Rate: ${vehicle.rental_rate}</p>
     </div>
     <Link to={`/book/${vehicle.vehicle_id}`}>
-    <button className="bg-blue-500 text-white px-4 py-2 mb-4 ml-6  rounded hover:bg-blue-800 transition duration-300 " onClick={() => handleVehicleClick(vehicle)}>
+    <button className="bg-base text-white px-4 py-2 mb-4 ml-6  rounded hover:bg-blue-800 transition duration-300 " onClick={() => handleVehicleClick(vehicle)}>
       {vehicle.availability ? 'Book Now' : 'Not Available'}
     </button></Link>
   </div>
@@ -41,7 +42,7 @@ const Explore = () => {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-8 bg-blue-500 text-white py-2 px-4">Explore Our Vehicles</h1>
+      <h1 className="text-3xl font-bold mb-8 bg-base text-white py-2 px-4">Explore Our Vehicles</h1>
       <div className="grid grid-cols-1 items-center md:grid-cols-2 lg:grid-cols-3 gap-6">
         {vehicles.map((vehicle) => (
           <VehicleCard key={vehicle.vehicle_id} vehicle={vehicle} handleVehicleClick={handleVehicleClick}/>
