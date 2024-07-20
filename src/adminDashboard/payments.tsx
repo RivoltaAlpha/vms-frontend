7a4f01370575b1ee1a041cd58ed6221e8c8de3f1
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-07-20 03:01:59 +0300

    more details

--- src/adminDashboard/payments.tsx
+++ src/adminDashboard/payments.tsx
@@ -32,8 +32,11 @@ const Payments = () => {
                     <th className="py-2">Payment status</th>
                     <th className="py-2">Amount</th>
                     <th className="py-2">Payment Date</th>
-                    <th className="py-2">User ID</th>
-                    <th className="py-2">Booking ID</th>
+                    <th className="py-2">Payment Method</th>
+                    <th className="py-2">User Name</th>
+                    <th className="py-2">Vehicle Booked</th>
+                    <th className="py-2">Vehicle Model</th>
+                    <th className="py-2">Booking Status</th>
                   </tr>
                 </thead>
                 <tbody>
@@ -48,8 +51,11 @@ const Payments = () => {
                       <td className="py-2 px-4">{payment.payment_status}</td>
                       <td className="py-2 px-4">${payment.amount}</td>
                       <td className="py-2 px-4">{new Date(payment.payment_date).toLocaleDateString()}</td>
-                      <td className="py-2 px-4">{payment.user_id}</td>
-                      <td className="py-2 px-4">{payment.booking_id}</td>
+                      <td className="py-2 px-4">{payment.payment_method}</td>
+                      <td className="py-2 px-4">{payment.user.username}</td>
+                      <td className="py-2 px-4">{payment.booking.vehicle.vehicleSpec.manufacturer}</td>
+                      <td className="py-2 px-4">{payment.booking.vehicle.vehicleSpec.model}</td>
+                      <td className="py-2 px-4">{payment.booking.booking_status}</td>
                     </tr>
                   ))
                 )}
